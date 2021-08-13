2f5c2284b8b7792bd9d9e4343e684e4ab56dd230
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-13 23:42:25 +0300

    Create do-while.cs

---
+++ do-while.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace do_while
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            //int i = 18; //Work ed at least once
+            do
+            {
+                listBox1.Items.Add("Hello World");
+                i++;
+            }
+            while (i <= 10);
+        }
+    }
+}
